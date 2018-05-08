69f4f611ccf03c6845d2e9665844f0f4c1680b57
Mark Stier <stier020@Marks-MacBook-Pro-2.local>
2018-05-08 08:27:28 -0500

    fix attachment bug

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 module.exports = {
   Document:require("./lib/document"),
-  Attachment:require("./lib/document"),
+  Attachment:require("./lib/attachment"),
   DocumentCursor:require("./lib/document-cursor"),
   ConnectionData:require("./lib/connection-data")
 }
